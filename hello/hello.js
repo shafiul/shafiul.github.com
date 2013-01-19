7f4101a8de07d91b0c0e6b3489fd11600941cda3
Shafiul Azam <ishafiul@gmail.com>
2013-01-19 10:47:02 +0600

    Fixed typos, Responsive topic added in presentaion

--- hello/hello.js
+++ hello/hello.js
@@ -1,6 +1,7 @@
-// Reference UI elements
+// Refer to UI elements
 var button = document.getElementById('myButton');
 var txtInput = document.getElementById('myTextInput');
+
 // Bind click event of the button with an event listener
 button.addEventListener('click', function(){
 	var text = txtInput.value;

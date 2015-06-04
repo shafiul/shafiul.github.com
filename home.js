6748ce7488a2e3752e3c055a4be85f7f5419a427
Shafiul Azam <ishafiul@gmail.com>
2015-06-04 21:23:11 +0600

    Added Firebase updates

---
+++ home.js
@@ -0,0 +1,30 @@
+function loadPersonalUpdates(){
+	// Get a database reference to our posts
+	var ref = new Firebase("https://shafiul.firebaseio.com/updates");
+	// Attach an asynchronous callback to read the data at our posts reference
+	ref.on("value", function(snapshot) {
+	  console.log('Firebase Date Loaded!')
+	  console.log(snapshot.val());
+	  populatePersonalUpdates(snapshot.val());
+	}, function (errorObject) {
+	  console.log("Firebase read failed: " + errorObject.code);
+	  populatePersonalUpdates(['Loading Failed :-(']);
+	});
+}
+
+function populatePersonalUpdates(data){
+	var markup = '';
+	totalPosts = 0;
+	for(i = data.length - 1; i >= 0; i--){
+		markup += '<p>' + data[i] + '</p>';
+
+		if(++totalPosts > 7){
+			break;
+		}
+	}
+	$('#personalUpdates').html(markup);
+}
+
+$(document).ready(function(){
+	loadPersonalUpdates();
+});

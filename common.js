b6953db2dd0a00d7be3251eb09b195556bf7281d
Shafiul Azam <ishafiul@gmail.com>
2015-11-06 00:19:23 -0600

    Added Projects Page

--- common.js
+++ common.js
@@ -2,13 +2,15 @@ var bigTitle = 'Shafiul Azam Chowdhury';
 var littleTitle = '<b>PhD Student and <a title=\"Graduate Research Assistant\">GRA</a></b> at <a title=\"Computer Science and Engineering Department at The University of Texas at Arlington\" href=\"http://cse.uta.edu\">CSE @ UTA</a>'
 var headerLinks= '<br/><a href="index.html">Home</a> | \
 <a href="bio.html">Bio</a> | \
-<a href="presentations.html">Presentations</a>'
+<a href="presentations.html">Presentations</a> | \
+<a href="projects.html">Projects</a>'
 
 var myFooter = '<p style="text-align: left; font-size: 10px;">\
           &copy; Nothing | \
           <a href="http://twitter.com/ishafiul">@ishafiul</a> |\
           <a href="http://bdhacker.wordpress.com">Blog</a> |\
-          <a href="presentations.html">Presentations</a>\
+          <a href="presentations.html">Presentations</a> |\
+          <a href="projects.html">Projects</a>\
         </p>';
 
 function commonDecoration(){

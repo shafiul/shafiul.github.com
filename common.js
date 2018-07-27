16d7cedff8ff811ce4fd089b5471ef5ca6d110ab
Shafiul Azam Chowdhury <ishafiul@gmail.com>
2018-07-27 13:29:21 -0500

    Sitewide firebase integration

--- common.js
+++ common.js
@@ -1,3 +1,57 @@
+function loadFire(data_url){
+  // Get a database reference to our posts
+  var ref = new Firebase(data_url);
+  // Attach an asynchronous callback to read the data at our posts reference
+  ref.on("value", function(snapshot) {
+    console.log('Firebase Date Loaded!')
+    console.log(snapshot.val());
+    populateFire(snapshot.val());
+  }, function (errorObject) {
+    console.log("Firebase read failed: " + errorObject.code);
+    populatePersonalUpdates(['Loading Failed :-('], div_id, max_len);
+  });
+}
+
+function populateFire(content){
+  var markup = '';
+  totalPosts = 0;
+
+
+  item_pre_markup = content.data_pre;
+  item_post_markup = content.data_post;
+  max_len = content.max_display;
+  data = content.data;
+  div_id = content.html_selector;
+
+  wrap_pre = content.pre;
+  wrap_post = content.post;
+
+  if(content.reverse){
+    data.reverse();
+  }
+
+  for(i = 0; i < data.length; ++i){
+
+    if(!data[i] || (typeof data[i] === 'object' && 'skip' in data[i] && data[i].skip))
+      continue;
+
+
+    if('item_decorator' in content){
+      data_item = window[content.item_decorator](data[i]);
+    }else{
+      data_item = data[i]
+    }
+
+    markup += item_pre_markup + data_item + item_post_markup;
+
+    if(++totalPosts >= max_len){
+      break;
+    }
+  }
+  $(div_id).html(wrap_pre + markup + wrap_post);
+}
+
+
 var bigTitle = 'Shafiul Azam Chowdhury';
 var littleTitle = '<b>PhD Student and <a title=\"Graduate Research Assistant\">GRA</a></b> at <a title=\"Computer Science and Engineering Department at The University of Texas at Arlington\" href=\"http://cse.uta.edu\">CSE @ UTA</a>'
 var headerLinks= '<br/><a href="index.html">Home</a> | \
--- home.js
+++ home.js
@@ -1,34 +1,18 @@
-function loadPersonalUpdates(){
-	// Get a database reference to our posts
-	var ref = new Firebase("https://shafiul.firebaseio.com/updates");
-	// Attach an asynchronous callback to read the data at our posts reference
-	ref.on("value", function(snapshot) {
-	  console.log('Firebase Date Loaded!')
-	  console.log(snapshot.val());
-	  populatePersonalUpdates(snapshot.val());
-	}, function (errorObject) {
-	  console.log("Firebase read failed: " + errorObject.code);
-	  populatePersonalUpdates(['Loading Failed :-(']);
-	});
-}
+function workDecorator(data){
+	markup = "<h5 class='card-title'>" + data.title + " at " + data.company + "</h5>";
+	markup += "<p class='card-text'>";
+	markup += "<i class='protech'>" + data.duration + " at " + data.location + "</i> <br><br> " + data.description + "<br><br>";
 
-function populatePersonalUpdates(data){
-	var markup = '';
-	totalPosts = 0;
-	for(i = data.length - 1; i >= 0; i--){
-
-		if(!data[i])
-			continue;
-
-		markup += '<li class = "list-group-item">' + data[i] + '</li>';
-
-		if(++totalPosts > 7){
-			break;
-		}
+	for(var i in data.technologies){
+		markup += "<span class='badge badge-secondary'>" + data.technologies[i] + "</span> ";
 	}
-	$('#personalUpdates').html(markup);
+
+	return markup + "<br><br> </p>";
 }
+
 
 $(document).ready(function(){
-	loadPersonalUpdates();
+	loadFire("https://shafiul.firebaseio.com/updates2");
+	loadFire("https://shafiul.firebaseio.com/work");
+	loadFire("https://shafiul.firebaseio.com/skills");
 });
